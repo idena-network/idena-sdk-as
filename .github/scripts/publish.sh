a431fc716e91b77bd719b7c7927e3c85752e0cec
sidenaio <st@idena.io>
2022-11-01 14:10:24 +0500

    Update publish.sh

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -6,24 +6,28 @@ cd ./sdk-core
 npm version $version --no-git-tag-version
 npm publish
 
+sleep 10
 
 cd ../bindgen
-yarn add idena-sdk-core@$version
+yarn add idena-sdk-core@$version --force
 npm version $version --no-git-tag-version
 yarn build
 npm publish
 
+sleep 10
 
 cd ../sdk-tests
 yarn build
 npm version $version --no-git-tag-version
 npm publish
 
+sleep 10
+
 cd ../sdk
 
-yarn add idena-sdk-core@$version
-yarn add idena-sdk-bindgen@$version
-yarn add idena-sdk-tests@$version
+yarn add idena-sdk-core@$version --force
+yarn add idena-sdk-bindgen@$version --force
+yarn add idena-sdk-tests@$version --force
 npm version $version --no-git-tag-version
 npm publish
 
